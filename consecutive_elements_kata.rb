f330e6c13e8128c98f084213140d0cc82ca97b4c
hshockney <shockney@atlanticbb.net>
2015-09-09 13:56:31 -0400

    added code used to make previous tests pass

--- consecutive_elements_kata.rb
+++ consecutive_elements_kata.rb
@@ -28,3 +28,19 @@ end
  
 #get the max value of consecutive identical elements
  count_of_consecutive_elements.max end
+ 
+ 
+ 
+ # def find_max_consecutive_identical_elements(any_array)
+#  if any_array.length == 0
+#     0
+#  elsif any_array[0] == any_array[1] && any_array[1] == any_array[2]
+#        3
+#  elsif any_array[0] == any_array[1]
+#        2
+#  elsif any_array[-2] == any_array[-1]
+#        2
+#  else
+#       1  
+#  end
+# end 
