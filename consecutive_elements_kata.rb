5aefda047ce2f77a7738ecd93dad341174ba53cb
hshockney <shockney@atlanticbb.net>
2015-09-09 13:54:31 -0400

    added notes to code to explain what is happening

--- consecutive_elements_kata.rb
+++ consecutive_elements_kata.rb
@@ -1,16 +1,30 @@
  def find_max_consecutive_identical_elements(any_array)
+  #array to hold values of consecutive identical arrays
   consecutive_elements = []
+  #array to hold the max values of the consecutive_elements array
   count_of_consecutive_elements = []
+  
+  #iterate through each element and set each one equal to value
   any_array.each do | value |
+  #if the consecutive_elements array is empty push in the value
 if consecutive_elements.length == 0
     consecutive_elements << value 
+	
+	#if the vale is equal to the last element in the consecutive_elements 
+	#array then push the value into the array.
 	elsif 
 	value == consecutive_elements[-1]
   consecutive_elements << value
-else
+
+ #push the count from the consecutive_elements array into the count_of_consecutive_elements 
+  else
     count_of_consecutive_elements << consecutive_elements.count
-consecutive_elements = []
+#reset arra to 0
+	consecutive_elements = []
 end
 end
+  #restate to make sure all values were included
   count_of_consecutive_elements << consecutive_elements.count
-  count_of_consecutive_elements.max end
+ 
+#get the max value of consecutive identical elements
+ count_of_consecutive_elements.max end
